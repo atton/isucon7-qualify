78c51b6c817d2d746f4343aae41953b7d946767b
atton <e115763@gmail.com>
2018-07-07 14:29:09 +0000

    mysql> set global innodb_flush_log_at_trx_commit = 2; (score: 101696)

--- db/init.sh
+++ db/init.sh
@@ -5,4 +5,5 @@ cd $DB_DIR
 
 mysql -uroot -e "DROP DATABASE IF EXISTS isubata; CREATE DATABASE isubata;"
 mysql -uroot isubata < ./isubata.sql
+mysql -uroot -e "set global innodb_flush_log_at_trx_commit = 2;"
 # zcat /home/isucon/isubata/bench/isucon7q-initial-dataset.sql.gz | sudo mysql isubata

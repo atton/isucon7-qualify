6e423be967b28e4de187720a7eb5b4a2717d1192
atton <e115763@gmail.com>
2018-07-11 04:43:09 +0000

    Tune mysql variables (score: 116718)

--- webapp/ruby/db/schema.rb
+++ webapp/ruby/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_120210) do
+ActiveRecord::Schema.define(version: 2018_07_11_044426) do
 
   create_table "channel", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "name", null: false

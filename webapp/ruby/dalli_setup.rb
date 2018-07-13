bab0b6c62ab832400e34f5343974a6970d18e0be
atton <e115763@gmail.com>
2018-07-13 05:51:34 +0000

    Set namespace string

--- webapp/ruby/dalli_setup.rb
+++ webapp/ruby/dalli_setup.rb
@@ -1,7 +1,7 @@
 require 'dalli'
 require 'pry'
 
-dalli = Dalli::Client.new
+dalli = Dalli::Client.new(ENV['ISUBATA_DB_HOST'], namespace: 'isubata')
 Dir.glob('/home/atton/isucon7-qualify/webapp/public/icons/*').each do |f|
   dalli.set(File.basename(f), File.read(f), 30 * 24 * 60 * 60)
 end

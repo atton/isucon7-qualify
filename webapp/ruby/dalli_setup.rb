b6de780cc38b30e117509bf0c3021b9f047aa497
atton <e115763@gmail.com>
2018-07-13 05:11:53 +0000

    Set TTL for memcached

--- webapp/ruby/dalli_setup.rb
+++ webapp/ruby/dalli_setup.rb
@@ -3,6 +3,6 @@ require 'pry'
 
 dalli = Dalli::Client.new
 Dir.glob('/home/atton/isucon7-qualify/webapp/public/icons/*').each do |f|
-  dalli.set(File.basename(f), File.read(f), nil, raw: true)
+  dalli.set(File.basename(f), File.read(f), 30 * 24 * 60 * 60, raw: true)
 end
 
